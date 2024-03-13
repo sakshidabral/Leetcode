46c4f43e7b1f9811596ec87b2ea5770ed5a6e796
sakshidabral <125342226+sakshidabral@users.noreply.github.com>
2024-03-13 23:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        for(int i=n/3;i<=n;i++){
-            int x=i, sum1=0, sum2=0;
-            for(int j=1;j<=x;j++)
-            sum1+=j;
-            for(int k=x;k<=n;k++)
-            sum2+=k;
-            if(sum1==sum2) return x;
+        /*1) 1->x = x(x+1)/2
+          2) x->n = (1->n)-(1->(x-1))
+          1 and 2 equal
+          x^2=n(n+1)/2  */
+        for(int i=1;i<=n;i++){
+            if((i*i)==(n*(n+1))/2) return i;
         }
         return -1;
     }
