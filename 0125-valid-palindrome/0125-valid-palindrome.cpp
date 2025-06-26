d879e545f8c8f6f0e5e7c3aa3eb60f852f9e0445
sakshidabral <125342226+sakshidabral@users.noreply.github.com>
2025-06-26 11:21:32 +0530

    Time: 0 ms (100%), Space: 10.2 MB (36.25%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str = "";
+        for(char c : s){
+            if(isalpha(c) || isdigit(c)) str += tolower(c);
+        }
+
+        int i = 0, j = str.size()-1;
+        while(i<=j){
+            if(str[i] != str[j]) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
