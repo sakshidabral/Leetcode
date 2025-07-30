7f05a0fdd2f0ce61de1ad08475f7a8d9a62243ea
sakshidabral <125342226+sakshidabral@users.noreply.github.com>
2025-07-30 13:16:02 +0530

    Time: 446 ms (14.06%), Space: 292.1 MB (9.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-
-    bool solve(string &s, int i, int j){
-        if(i >= j) return true;
-
-        if(s[i] == s[j]) return solve(s, i+1, j-1);
-
-        return false;
-    }
-
     string longestPalindrome(string s) {
-        //Accolite, Amazon, Groupon, MakeMyTrip, Microsoft, Qualcomm, Samsung, Visa, Walmart, Zoho
         int n =  s.size();
-        
         int maxLen = INT_MIN;
         int sp = 0; // starting point
+        vector<vector<int>> dp(n, vector<int>(n,-1));
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(solve(s,i,j) == true){
+                if(solve(s,i,j,dp) == true){
                     if(j-i+1 > maxLen){
                         maxLen = j-i+1;
                         sp = i;
@@ -30,5 +20,13 @@ public:
         return s.substr(sp,maxLen);
     }
 
+    bool solve(string &s, int i, int j, vector<vector<int>>& dp){
+        if(i >= j) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i] == s[j]) return dp[i][j] = solve(s, i+1, j-1, dp);
+
+        return dp[i][j] = 0;
+    }
     
 };
